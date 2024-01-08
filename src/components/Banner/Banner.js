c801a35bbb8b8293c32c50e2e9bafb66cae3e190
ArFran17 <20041244@itdurango.edu.mx>
2024-01-08 17:52:46 -0600

    Primer Modificacion

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+       <div className="banner-container">
+        <h1>Zapateria Tepehuanes</h1>
+        <p>El mejor Calzado para ti</p>
+        </div>
+        </div>
+  )
+}
+export default Banner;
